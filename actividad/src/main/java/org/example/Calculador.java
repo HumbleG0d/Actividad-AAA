f5adfc7843c04944a96e819737a6b44949dd2321
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-03 16:35:13 -0500

    Add Calculador Class

---
+++ actividad/src/main/java/org/example/Calculador.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Calculador {
+  public int add(int numeroA, int numeroB) {
+    if ((numeroA > 0 && numeroB > 0 && numeroA > Integer.MAX_VALUE - numeroB) ||
+            (numeroA < 0 && numeroB < 0 && numeroA < Integer.MIN_VALUE - numeroB)) {
+      throw new ArithmeticException("Overflow al sumar " + numeroA + " y " + numeroB);
+    }
+    return numeroA + numeroB;
+  }
+
+  public int substract(int numeroA, int numeroB) {
+    if(numeroA == Integer.MIN_VALUE || numeroB == Integer.MIN_VALUE){
+      throw new IllegalArgumentException();
+    }
+    return numeroA - numeroB;
+  }
+
+  public int multiply(int numeroA, int numeroB) {
+    if(numeroA == Integer.MAX_VALUE || numeroB == Integer.MAX_VALUE){
+      throw new IllegalArgumentException();
+    }
+    return numeroA * numeroB;
+  }
+
+  public double divide(int numeroA, int numeroB) {
+    if (numeroB == 0) {
+      throw new ArithmeticException("Division por cero");
+    }
+    return (double) numeroA / numeroB;
+  }
+
+}
