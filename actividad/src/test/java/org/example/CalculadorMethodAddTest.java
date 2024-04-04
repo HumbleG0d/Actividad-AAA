68f4406627fb53ddef86de8c1b8c82cf57d83d26
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-03 21:57:36 -0500

    Add the test of the method add

--- actividad/src/test/java/org/example/CalculadorMethodAddTest.java
+++ actividad/src/test/java/org/example/CalculadorMethodAddTest.java
@@ -3,7 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 public class CalculadorMethodAddTest {
   @Test //Test pata dos numeros positivos.
