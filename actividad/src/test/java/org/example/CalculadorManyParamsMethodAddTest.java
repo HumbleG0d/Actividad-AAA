2de6b8bdf37863cd21d1e3d80f2fde245deba2a2
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-03 22:27:17 -0500

    Change value CsvSource

--- actividad/src/test/java/org/example/CalculadorManyParamsMethodAddTest.java
+++ actividad/src/test/java/org/example/CalculadorManyParamsMethodAddTest.java
@@ -13,11 +13,11 @@ public class CalculadorManyParamsMethodAddTest {
           "2 , 3 , 5", //Test para dos numeros positivos.
           "-2 , -3 , -5", //Test para dos numeros negativos.
           "0 , 0 , 0 ", //Test para dos ceros.
-          "2147483647 , 1 , 214748648", //Test para el valor maximo de un entero.
+          "2147483647 , 1 , -2147483648", //Test para el valor maximo de un entero.
           "-2 , 2147483647 , 2147483645", // Test para el valor minimo de un entero.
           "-1 , -2147483646 , -2147483647" // Test para el valor minimo de un entero.
   })
-  public void testSuma_ShouldReturnCorrectAdd(int numberA , int numberB , int expected){
+  public void testAdd_ShouldReturnCorrectAdd(int numberA , int numberB , int expected){
     /*Arrange:
     * Datos de entrada -> Argumentos a y b.
     * Entorno de prueba -> Instancia del objeto Calculador
