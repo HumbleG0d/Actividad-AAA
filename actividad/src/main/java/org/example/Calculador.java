911385ccd304439509166ef6d8d74c7779efcc6b
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-03 21:56:09 -0500

    Change the coditional of the metod add and substract

--- actividad/src/main/java/org/example/Calculador.java
+++ actividad/src/main/java/org/example/Calculador.java
@@ -3,15 +3,15 @@ package org.example;
 public class Calculador {
   public int add(int numeroA, int numeroB) {
     if ((numeroA > 0 && numeroB > 0 && numeroA > Integer.MAX_VALUE - numeroB) ||
-            (numeroA < 0 && numeroB < 0 && numeroA < Integer.MIN_VALUE - numeroB)) {
+    (numeroA < 0 && numeroB < 0 && numeroA < Integer.MIN_VALUE - numeroB)) {
       throw new ArithmeticException("Overflow al sumar " + numeroA + " y " + numeroB);
     }
     return numeroA + numeroB;
   }
 
   public int substract(int numeroA, int numeroB) {
-    if(numeroA == Integer.MIN_VALUE || numeroB == Integer.MIN_VALUE){
-      throw new IllegalArgumentException();
+    if( (numeroA == Integer.MIN_VALUE && numeroB < 0) || (numeroB == Integer.MIN_VALUE && numeroA < 0 )){
+      throw new ArithmeticException("Overflow al restar " + numeroA + " y " + numeroB);
     }
     return numeroA - numeroB;
   }
