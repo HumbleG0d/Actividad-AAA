cfb932f441afd1e00133641ee61f6aaa9316cd61
HumbleG0d <humbleg0d7642@gmail.com>
2024-04-03 22:28:19 -0500

    Add test the method substract

---
+++ actividad/src/test/java/org/example/CalculadorManyParamsMethodSubstracTest.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CalculadorManyParamsMethodSubstracTest {
+    @ParameterizedTest
+    @CsvSource({
+            "2 , 3 , -1", //Test para dos numeros positivos.
+            "-2 , -3 , 1", //Test para dos numeros negativos.
+            "0 , 0 , 0 ", //Test para dos ceros.
+            "2147483647 , 1 , 2147483646", //Test para el valor maximo de un entero.
+            "-2 , 2147483647 , 2147483647", // Test para el valor minimo de un entero.
+            "-1 , -2147483646 , 2147483645" // Test para el valor minimo de un entero.
+    })
+  public void testSubstract_ShouldReturnCorrectAdd(int numberA , int numberB , int expected){
+    /*Arrange:
+    * Datos de entrada -> Argumentos a y b.
+    * Entorno de prueba -> Instancia del objeto Calculador
+    * */
+
+    Calculador calculador = new Calculador();
+    //Captura de excepciones
+    try{
+      //Act
+      int resultado = calculador.substract(numberA , numberB);
+
+      //Assert
+      assertEquals(expected , resultado);
+
+    }catch (ArithmeticException e){
+      System.out.print("Error: " + e.getMessage());
+    }
+
+  }
+
+}
